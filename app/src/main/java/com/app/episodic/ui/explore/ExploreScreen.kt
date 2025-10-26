43aa2c183695b3b017d886c65c992e8fc4842b25
Ludoviko451 <luisangelquirosflorez@gmail.com>
2025-10-26 15:39:10 -0500

    - Eliminando importancion no usada

--- app/src/main/java/com/app/episodic/ui/explore/ExploreScreen.kt
+++ app/src/main/java/com/app/episodic/ui/explore/ExploreScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.app.episodic.ui.components.LoadingView
